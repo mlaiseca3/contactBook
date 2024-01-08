41339d7c2781169fba36f0dbb9fb663613800be1
Mario L <mariolaisecaruiz@gmai.com>
2024-01-07 22:50:56 -0600

    added first route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+	createBrowserRouter,
+	RouterProvider
+} from "react-router-dom";
+import Root from './routes/root';
+
 import './index.css'
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <div> Hello, world </div>,
+	},
+
+])
+
+	
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+		<RouterProvider router= {router} />
   </React.StrictMode>,
 )
